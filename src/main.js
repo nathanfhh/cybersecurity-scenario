bc3f576bcf99a849835a7269a10efe2f0e8b1dd6
傅暄桓 <t13762@ms.sltung.com.tw>
2024-11-16 00:24:53 +0800

    refactor: reformat code add: introduce dark mode

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-
-
 import {createApp} from 'vue'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import {createPinia} from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 const app = createApp(App)
 const pinia = createPinia()
